7a6419826c6a7d8cb19ffbc069cd81378a479809
rebmir <rebeccahmiruka52@gmail.com>
2025-06-23 08:42:56 +0300

    Updated

--- my-front-end-app/vite.config.js
+++ my-front-end-app/vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: './', 
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
